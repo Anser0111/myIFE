c5cc4d92665bcb75a1bca9c3cebbaaa24b8778ac
Anser0111 <1052521358@qq.com>
2020-11-17 13:23:11 +0800

    格式化了一下

--- day16/计算器/myScript.js
+++ day16/计算器/myScript.js
@@ -5,31 +5,31 @@ var subtract = x1 - x2;
 var multiply = x1 * x2;
 var divide = x1 / x2;
 function calculation1() {
-    x1 = document.getElementById("first-number").value;
-    x2 = document.getElementById("second-number").value;
-    add = parseFloat(x1) + parseFloat(x2);
-    document.getElementById("result").innerHTML = add;
+  x1 = document.getElementById("first-number").value;
+  x2 = document.getElementById("second-number").value;
+  add = parseFloat(x1) + parseFloat(x2);
+  document.getElementById("result").innerHTML = add;
 }
 function calculation2() {
-    x1 = document.getElementById("first-number").value;
-    x2 = document.getElementById("second-number").value;
-    subtract = parseFloat(x1) - parseFloat(x2);
-    document.getElementById("result").innerHTML = subtract;
+  x1 = document.getElementById("first-number").value;
+  x2 = document.getElementById("second-number").value;
+  subtract = parseFloat(x1) - parseFloat(x2);
+  document.getElementById("result").innerHTML = subtract;
 }
 function calculation3() {
-    x1 = document.getElementById("first-number").value;
-    x2 = document.getElementById("second-number").value;
-    multiply = parseFloat(x1) * parseFloat(x2);
-    document.getElementById("result").innerHTML = multiply;
+  x1 = document.getElementById("first-number").value;
+  x2 = document.getElementById("second-number").value;
+  multiply = parseFloat(x1) * parseFloat(x2);
+  document.getElementById("result").innerHTML = multiply;
 }
 function calculation4() {
-    x1 = document.getElementById("first-number").value;
-    x2 = document.getElementById("second-number").value;
-    divide = parseFloat(x1) / parseFloat(x2);
-    document.getElementById("result").innerHTML = divide;
+  x1 = document.getElementById("first-number").value;
+  x2 = document.getElementById("second-number").value;
+  divide = parseFloat(x1) / parseFloat(x2);
+  document.getElementById("result").innerHTML = divide;
 }
 
-document.getElementById("add-btn").addEventListener('click', calculation1);
-document.getElementById("minus-btn").addEventListener('click', calculation2);
-document.getElementById("times-btn").addEventListener('click', calculation3);
-document.getElementById("divide-btn").addEventListener('click', calculation4);
+document.getElementById("add-btn").addEventListener("click", calculation1);
+document.getElementById("minus-btn").addEventListener("click", calculation2);
+document.getElementById("times-btn").addEventListener("click", calculation3);
+document.getElementById("divide-btn").addEventListener("click", calculation4);
